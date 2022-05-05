2081af8f5bbf9f7350220787cbab3de189e0507b
Weng,David D <d.weng@student.fontys.nl>
2022-05-05 16:03:24 +0200

    lol

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const stackConfig = new pulumi.Config('backend-api-aws');
 // Create an AWS resorm -r node_modulesurce (S3 Bucket)
 // Create an AWS resource (S3 Bucket)
 
-async function pulumiProgram()  {
+export const pulumiProgram = async () => {
     const bucket = new aws.s3.Bucket(stackConfig.require('purposeName'));
 // Export the name of the bucket
     return {
